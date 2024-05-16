dad95f8da26bf70801175d7ebb98c7327f742897
avallete <andrew@snaplet.dev>
2024-05-16 15:29:02 +0200

    fix: db diagrams

--- components/recipes/SlackAppERD.tsx
+++ components/recipes/SlackAppERD.tsx
@@ -1,14 +1,25 @@
-import * as React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useTheme } from "nextra-theme-docs";
-
 
 export const SlackAppERD = () => {
     const { resolvedTheme } = useTheme();
+    const [iframeSrc, setIframeSrc] = useState('');
+    
+    useEffect(() => {
+        // This code now runs only on the client side
+        if (resolvedTheme === "dark") {
+            setIframeSrc("https://dbdiagram.io/e/66460034f84ecd1d225b7d68/66460063f84ecd1d225b82f2");
+        } else {
+            setIframeSrc("https://dbdiagram.io/e/6644dfeff84ecd1d2244fbde/6644e883f84ecd1d2245ac3c");
+        }
+    }, [resolvedTheme]);
 
-    if (resolvedTheme === "dark") {
-    return (<iframe width="880" height="720" src='https://dbdiagram.io/e/66460034f84ecd1d225b7d68/66460063f84ecd1d225b82f2'></iframe>)
-    }
-
-    return  (<iframe width="880" height="720" src='https://dbdiagram.io/e/6644dfeff84ecd1d2244fbde/6644e883f84ecd1d2245ac3c'> </iframe>)
+    return (
+        <iframe
+            width="880"
+            height="720"
+            src={iframeSrc}
+            title="DB Diagram"
+        ></iframe>
+    );
 };
-  
